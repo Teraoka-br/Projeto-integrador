e7987d565d71a30744b688db0615eacfbc4d87a9
Teraoka-br <fabricio.teraoka@gmail.com>
2022-08-06 10:21:27 -0300

    adicionando icones no menu central

--- src/components/MenuCentral.jsx
+++ src/components/MenuCentral.jsx
@@ -1,16 +1,25 @@
 import MenuCentralItem from "./MenuCentralItem";
 import cliente from "../assets/icones/cliente.png";
+import engrenagem from "../assets/icones/engrenagem.png"
+import processos from "../assets/icones/processos.png"
+import tarefas from "../assets/icones/tarefas.png"
+
 export default function MenuCentral() {
   return (
-    <div class="grid">
-      <div class="g-col-6"><MenuCentralItem icone={cliente} titulo="Cliente" to="/clientes/cadastrar" /></div>
-      <div class="g-col-6"><MenuCentralItem icone={cliente} titulo="Cliente"/></div>
+    <div class="grid" >
 
-      <div class="g-col-6"><MenuCentralItem icone={cliente} titulo="Cliente"/></div>
-      <div class="g-col-6"><MenuCentralItem icone={cliente} titulo="Cliente"/></div>
+
+      <div class="g-col-4"><MenuCentralItem icone={cliente} titulo="Cliente" to="/clientes/cadastrar" /></div>
+      <div class="g-col-4"><MenuCentralItem icone={engrenagem} titulo="Cliente"/></div>
+
+      <div class="g-col-4"><MenuCentralItem icone={processos} titulo="Cliente"/></div>
+      <div class="g-col-4"><MenuCentralItem icone={tarefas} titulo="Cliente"/></div>
+
+
     </div>
-
+    
   );
+  
 }
 
 
